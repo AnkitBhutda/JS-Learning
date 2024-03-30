b9c0a55acb0785cb484c22272c0d58ab15f3f5bf
AnkitBhutda <98318271+AnkitBhutda@users.noreply.github.com>
2024-03-30 20:35:05 +0000

    Datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 +3); allow in browser not in node js
+
+console.log(3 
+    + 
+    3); // not good practice as code readability should alwasys be high 
+
+console.log('Ankit');
+
+// Datatypes
+
+let name = 'Ankit' // String
+let age = 25 // number
+let isLoggedIn = true // boolean
+let state;
+
+// nuber => 2 to power of 53
+// bigint
+// string => '' or ""
+// boolena => true or false
+// null => Standalone value
+// undefined => variable declare but value not define
+// symbol => unique identifier
+
+// Object 
+
+console.log(typeof 'Ankit');
+console.log(typeof 23);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
